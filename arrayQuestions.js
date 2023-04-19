1fda50aac59d1851717df74d724a2ba644ab146b
Adiination <Aditya.sasaram11@gmail.com>
2023-04-19 14:05:04 +0530

    table function

--- arrayQuestions.js
+++ arrayQuestions.js
@@ -1,44 +1,54 @@
 
 //Max occurence js Function =
 
-function mostFrequent(arr) {
-    let count = {};
-    let maxCount = 0;
-    let maxItem = null;
+// function mostFrequent(arr) {
+//     let count = {};
+//     let maxCount = 0;
+//     let maxItem = null;
   
-    for (let item of arr) {
-      if (!count[item]) {
-        count[item] = 1;
-      } else {
-        count[item]++;
-      }
-      if (count[item] > maxCount) {
-        maxCount = count[item];
-        maxItem = item;
-      }
-    }
+//     for (let item of arr) {
+//       if (!count[item]) {
+//         count[item] = 1;
+//       } else {
+//         count[item]++;
+//       }
+//       if (count[item] > maxCount) {
+//         maxCount = count[item];
+//         maxItem = item;
+//       }
+//     }
   
-    return maxItem;
-  }
-let arr = [2,3,4,2,4,2,5,62,6];
-let v = mostFrequent(arr); 
-console.log(v); 
+//     return maxItem;
+//   }
+// let arr = [2,3,4,2,4,2,5,62,6];
+// let v = mostFrequent(arr); 
+// console.log(v); 
 
 
-//function that returns largest word of a string:
+// //function that returns largest word of a string:
 
-function longest (string){
-  let words = string.split(" ");
-  let longestWord = "";
-  for (let i = 0; i<words.length ;i++){
-    if (words[i].length>longestWord.length){
-      longestWord = words[i];
+// function longest (string){
+//   let words = string.split(" ");
+//   let longestWord = "";
+//   for (let i = 0; i<words.length ;i++){
+//     if (words[i].length>longestWord.length){
+//       longestWord = words[i];
   
-    }
+//     }
 
-  }
-  return longestWord;
+//   }
+//   return longestWord;
+// }
+
+// const str = longest("A quick brown fox jumped over a lazy dog");
+//  console.log(str);
+
+//  table of a give number 
+const table =  (n) => {
+  for(let i = 1; i <= 10; i++){
+    console.log(`${n} x ${i} = ${n * i}`)
 }
 
-const str = longest("A quick brown fox jumped over a lazy dog");
- console.log(str);
+}
+
+table(3);
