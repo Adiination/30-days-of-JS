9608ff2c8e7515b818b145ee7a2f0b53994bc164
Adiination <Aditya.sasaram11@gmail.com>
2023-05-11 18:46:17 +0530

    string question and solution

--- stringQuestions.js
+++ stringQuestions.js
@@ -1,4 +1,4 @@
-//How do you check if a string contains a specific substring in JavaScript?
+// How do you check if a string contains a specific substring in JavaScript?
 // const str = "The quick brown fox jumps over the lazy dog";
 // const substr = "brown";
 // const containsSubstr = str.includes(substr);
