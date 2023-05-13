e9212448a46b7745748a58554e98effe7e2eaaff
Adiination <Aditya.sasaram11@gmail.com>
2023-05-13 23:48:53 +0530

    to get the number of times a number comes in an array

--- arrayQuestions.js
+++ arrayQuestions.js
@@ -128,4 +128,21 @@
 // let b = [12,2,3,4,56,43,78];
 // console.log(containsDuplicate(b));
 
+//9). To return how many of of the n number are there in an array :
+
+// function countn(array,Num){
+//     var count = 0;
+//     for(var i = 0; i < array.length; i++){
+//         if(array[i]==Num){
+//             count++;
+//         }
+
+// }
+// return count;
+// }
+
+
+// console.log(countn([2,3,4,2,3,5,5,2],4));
+
+
 
